39cf9b4386d330e32f4c53c8dee10ec8c6ebcb0f
redraycn <redraycn@gmail.com>
2025-04-11 10:06:36 +0000

    第一次提交

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import Cookies from 'js-cookie';
 
 export function middleware(request: NextRequest) {
-  const token = localStorage.getItem('token'); // Assuming the token is stored as 'token'
+  const token = request.cookies.get('token')?.value;
 
   if (!token && !['/login', '/register'].includes(request.nextUrl.pathname)) {
+
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
