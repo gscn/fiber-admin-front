e56c8c2cc41829f04f7436aff8330af850f0daa7
redraycn <redraycn@gmail.com>
2025-04-11 10:19:25 +0000

    修复错误

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "3000-idx-fiber-admin-1744344389291.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev"
+  ],
 };
 
 export default nextConfig;
